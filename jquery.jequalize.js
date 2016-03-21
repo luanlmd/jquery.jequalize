f2e1f5269642251772fc86fcb5413642e7d90162
Luan Almeida <luanlmd@gmail.com>
2016-03-21 09:59:56 -0300

    updating code

--- jquery.jequalize.js
+++ jquery.jequalize.js
@@ -1,24 +1,28 @@
-jQuery.fn.jequalize = function(x) {
-	function equalize(e)
-	{
-		var h = 0;
-		e.each(function()
+(function($){
+    "use strict";
+    $.fn.jequalize = function(x)
+    {
+		function equalize(e)
 		{
-			oh = $(this).outerHeight();
-			if (oh > h) { h = oh; }
-		});
-		e.height(h);
-	}
+			var h = 0;
+			e.each(function()
+			{
+				var oh = $(this).outerHeight();
+				if (oh > h) { h = oh; }
+			});
+			e.height(h);
+		}
 
-	if (x == undefined)
-	{
-		equalize($(this));
+		if (x == undefined)
+		{
+			equalize($(this));
+			return this;
+		}
+
+		for(var i = 0; i < $(this).length; i+=x)
+		{
+			equalize($(this).slice(i, i + x))
+		}
 		return this;
-	}
-	
-	for(i = 0; i < $(this).length; i+=x)
-	{
-		equalize($(this).slice(i, i + x))
-	}
-	return this;
-};
+	};
+})(jQuery);
